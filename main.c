13be459b49bea34cc26194555c1dc733de7398cc
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2019-04-12 11:55:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "declaraciones.h"
 
 int main(){
-  GList ListaDePersonas=lectura_archivo();
-  for (size_t i = 0; i< 5; i++) {
-    printf("%s %d %s\n", ListaDePersonas->dato->nombre, ListaDePersonas->dato->edad,ListaDePersonas->dato->lugarDeNacimiento);
-  }
+  GList ListaDePersonas=lectura_archivo(leer_persona);
+
+  filtrar_lista(ListaDePersonas, mayor_de_edad,copiar_nodo,"mayores_de_edad.txt", escribir_persona);
+  filtrar_lista(ListaDePersonas, empieza_con_a,copiar_nodo,"nombrescona.txt", escribir_persona);
+
+  mapear_lista(ListaDePersonas, ocultar_nombre, copiar_nodo, "ocultanombre.txt", escribir_persona);
+  mapear_lista(ListaDePersonas, aumentar_edad, copiar_nodo, "aumentaedad.txt", escribir_persona);
+
+  glist_destruir(ListaDePersonas,eliminar_persona);
+
   return 1;
 }
